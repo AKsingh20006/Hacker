a7993d0d1d82c05201da0c4ee6e699b18ea6b9c8
AKsingh20006 <ayushsingh70554@gmail.com>
2026-03-15 23:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNNpeAb0FPjmNNbxwZeVkyP",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
